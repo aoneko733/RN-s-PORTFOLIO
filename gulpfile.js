5e591bccfac0e618c08a57f47e650a3e9ede4d34
DESKTOP-9LU36LJ\Owner <aoneko733@gmail.com>
2018-10-28 16:44:43 +0900

    gulp導入

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+const gulp = require('gulp'),
+sass = require('gulp-sass'),
+autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+  gulp.src('./scss/*.scss')
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
+    .pipe(autoprefixer({
+      browsers: ["last 2 versions"],
+      cascade: false
+    }))
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./scss/*.scss', ['sass']);
+});
